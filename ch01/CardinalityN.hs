87936037802e5a823cc05d78ffb6fdd1f2c473e4
waddlaw <ingronze@gmail.com>
2019-10-16 16:04:23 +0900

    ch01

--- ch01/CardinalityN.hs
+++ ch01/CardinalityN.hs
@@ -14,21 +14,17 @@ type family Cardinality (n :: Nat) (a :: Type) :: Nat where
   Cardinality _ Void         = 0
   Cardinality _ ()           = 1
   Cardinality _ Bool         = 2
+  Cardinality n (a, b)       = Cardinality n a * Cardinality n b
+  Cardinality n (Maybe a)    = 1 + Cardinality n a
+  Cardinality n (Either a b) = Cardinality n a + Cardinality n b
+  Cardinality n (a -> b)     = Cardinality n b ^ Cardinality n a
   Cardinality 0 _            = 0
-  Cardinality n (a, b)       = Cardinality (n-1) a * Cardinality (n-1) b
-  Cardinality n (Either a b) = Cardinality (n-1) a + Cardinality (n-1) b
-  Cardinality n (a -> b)     = Cardinality (n-1) b ^ Cardinality (Cardinality (n-1) b - 1) a
   Cardinality n [a]          = 1 + Cardinality (n-1) a * Cardinality (n-1) [a]
 
 {-
-λ natVal (Proxy @(Cardinality 0 [Bool]))
-0
-λ natVal (Proxy @(Cardinality 1 [Bool]))
-1
-λ natVal (Proxy @(Cardinality 2 [Bool]))
-3
-λ natVal (Proxy @(Cardinality 3 [Bool]))
-7
 λ natVal (Proxy @(Cardinality 4 [Bool]))
 15
+
+λ> natVal (Proxy @(Cardinality 0 (Either Bool (Bool, Maybe Bool) -> Bool)))
+256
 -}
